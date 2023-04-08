aa50b895f9831bd68a71c93432549b25ad471f60
tee <aderetioluwatoyin@gmail.com>
2023-04-08 22:44:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,147 @@
+let firstIcon = document.getElementById('icon-one')
+let secondIcon = document.getElementById('icon-two')
+let thirdIcon = document.getElementById('icon-three')
+let fourthIcon = document.getElementById('icon-four')
+
+let calcBody = document.querySelector('.calculator')
+let calc = document.querySelector('.calc')
+let themee = document.querySelector('.themee')
+let del = document.querySelector('.del')
+let reset = document.querySelector('.reset')
+let equal = document.querySelector('.equal')
+let displayInput = document.querySelector('.display-inp')
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let but = document.getElementById('but')
+let butt = document.getElementById('butt')
+let buttt = document.getElementById('buttt')
+let buut = document.getElementById('buut')
+let buuut = document.getElementById('buuut')
+let buuuut = document.getElementById('buuuut')
+let buuuuut = document.getElementById('buuuuut')
+let buts = document.getElementById('buts')
+let butss = document.getElementById('butss')
+let butsss = document.getElementById('butsss')
+let butssss = document.getElementById('butssss')
+let butz = document.getElementById('butz')
+let butzz = document.getElementById('butzz')
+let butzzz = document.getElementById('butzzz')
+let butzzzz = document.getElementById('butzzzz')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// firstIcon.addEventListener('click',()=>{
+    
+//     secondIcon.style.display = 'block'
+//     // calcBody.style.backgroundColor='hsl(0, 0%, 93%)'
+//     console.log('click')
+//     firstIcon.style.display = 'none'
+    
+// } )
+
+secondIcon.addEventListener('click', ()=>{
+  secondIcon.style.display ='none'
+  thirdIcon.style.display='block'
+  calcBody.style.backgroundColor = 'hsl(0, 5%, 81%)'
+  calc.style.color = 'black'
+  themee.style.color = 'black'
+  del.style.backgroundColor = 'hsl(176, 100%, 44%)'
+  reset.style.backgroundColor = 'hsl(176, 100%, 44%)'
+  displayInput.style.backgroundColor = 'hsl(0, 0%, 93%)'
+  displayInput.style.color = 'black'
+  one.style.color ='black'
+  two.style.color ='black'
+  three.style.color='black'
+
+})
+thirdIcon.addEventListener('click', ()=>{
+    thirdIcon.style.display ='none'
+    fourthIcon.style.display ='block'
+    fourthIcon.style.paddingLeft = '99px'
+    calcBody.style.backgroundColor = 'hsl(268, 75%, 9%)'
+    calc.style.color = 'hsl(52, 100%, 62%)'
+    themee.style.color = 'hsl(52, 100%, 62%)'
+    del.style.backgroundColor = 'hsl(281, 89%, 26%)'
+    reset.style.backgroundColor = 'hsl(281, 89%, 26%)'
+    one.style.color ='hsl(52, 100%, 62%)'
+    two.style.color ='hsl(52, 100%, 62%)'
+    three.style.color='hsl(52, 100%, 62%)'
+    displayInput.style.backgroundColor = 'hsl(268, 71%, 12%)'
+    displayInput.style.color = 'hsl(52, 100%, 62%)'
+    equal.style.backgroundColor = 'hsl(176, 100%, 44%)'
+    equal.style.Color = 'black'
+    // but.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // but.style.color = 'hsl(52, 100%, 62%)'
+    // butt.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butt.style.color = 'hsl(52, 100%, 62%)'
+    // buttt.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buttt.style.color = 'hsl(52, 100%, 62%)'
+    // buut.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buut.style.color = 'hsl(52, 100%, 62%)'
+    // buuut.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buuut.style.color = 'hsl(52, 100%, 62%)'
+    // buuuut.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buuuut.style.color = 'hsl(52, 100%, 62%)'
+    // buuuuut.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buuuuut.style.color = 'hsl(52, 100%, 62%)'
+    // buts.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // buts.style.color = 'hsl(52, 100%, 62%)'
+    // butss.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butss.style.color = 'hsl(52, 100%, 62%)'
+    // butsss.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butsss.style.color = 'hsl(52, 100%, 62%)'
+    // butssss.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butssss.style.color = 'hsl(52, 100%, 62%)'
+    // butz.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butz.style.color = 'hsl(52, 100%, 62%)'
+    // butzz.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butzz.style.color = 'hsl(52, 100%, 62%)'
+    // butzzz.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butzzz.style.color = 'hsl(52, 100%, 62%)'
+    // butzzzz.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    // butzzzz.style.color = 'hsl(52, 100%, 62%)'
+
+
+    
+
+
+
+
+})
+
+fourthIcon.addEventListener('click', ()=>{
+    fourthIcon.style.display = 'none'
+    secondIcon.style.display = 'block'
+    calcBody.style.backgroundColor = ' hsl(223, 31%, 20%);'
+    calc.style.color = 'white'
+    themee.style.color = 'white'
+    del.style.backgroundColor = 'hsl(225, 21%, 49%)'
+    reset.style.backgroundColor = 'hsl(225, 21%, 49%)'
+    displayInput.style.backgroundColor = 'hsl(224, 36%, 15%)'
+    displayInput.style.color = 'white'
+    one.style.color ='white'
+    two.style.color ='white'
+    three.style.color='white'
+    equal.style.backgroundColor = 'hsl(29, 93%, 50%)'
+
+
+ })
